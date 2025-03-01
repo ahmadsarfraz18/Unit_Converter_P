243a2769d97ffe3242823e5f234c500b20b82d91
ahmadsarfraz18 <160363481+ahmadsarfraz18@users.noreply.github.com>
2025-03-01 12:13:53 +0500

    Add files via upload

---
+++ new.py
@@ -0,0 +1,33 @@
+# streamlit ko import karenge :
+import streamlit as st # pip install streamlit
+# function banayenge jis mien hum 3 cheezen add karenge
+# values = user ka input , from_unit = jisko convert karna h , to_unit= jis mien convert karna h 
+def Convert_units(values, from_unit , to_unit):
+    conversion_formula = {
+                "meters": {"kilometers": 0.001, "centimeters": 100, "feet": 3.28084},
+        "kilometers": {"meters": 1000, "miles": 0.621371},
+        "miles": {"kilometers": 1.60934, "meters": 1609.34},
+        "feet": {"meters": 0.3048, "centimeters": 30.48},
+        "centimeters": {"meters": 0.01, "feet": 0.0328084},
+    }
+
+    if from_unit == to_unit:
+        return values
+    # Calculation yahan sy start krn gay
+    return values * conversion_formula.get(from_unit, {}).get(to_unit, {})
+    st.title("Simple Unit Converter")
+    st.sidebar.header ("Select Conversion")
+
+    # Let's define from_unit...
+
+    from_unit = st.sidebar.selectbox ("From", ["meters", "kilometers", "miles", "centimeters", "feet"])
+    to_unit = st.sidebar.selectbox ("To", ["meters", "kilometers", "miles", "centimeters", "feet"])
+    values = st.sidebar.number_input ("Enter your value", min_value = 0.0, format = "%.2f")
+
+    # Now we will run the condition...
+
+    if st.sidebar.button ("Convert"):
+        result = Convert_units (values, to_unit, from_unit)
+        st.success ("")
+
+    
